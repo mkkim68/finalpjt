a670140911c8fed1995cfdb76c4169c07f5b80f0
김민경 <infinitelove367@gmail.com>
2024-05-16 14:05:47 +0900

    0516_mk_create_model

--- final_pjt_back/accounts/models.py
+++ final_pjt_back/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from banks.models import Deposit, Saving
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    deposit = models.ManyToManyField(Deposit, blank=True)
+    saving = models.ManyToManyField(Saving, blank=True)
--- final_pjt_back/final_pjt_back/settings.py
+++ final_pjt_back/final_pjt_back/settings.py
@@ -68,6 +68,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    # Permission
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 
 }
 
