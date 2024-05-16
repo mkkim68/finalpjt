bb35a0b1138538dacce7c3d3b1b0a2b714e0d7d3
김민경 <infinitelove367@gmail.com>
2024-05-16 17:48:28 +0900

    0516_mk_create_FIN_API_views

--- final_pjt_back/banks/serializers.py
+++ final_pjt_back/banks/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import Deposit, Saving
+from .models import Deposit, DepositOptions, Saving, SavingOptions
 
 class DepositSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deposit
         fields = '__all__'
+
+class DepositOptionsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = DepositOptions
+        fields = '__all__'
+        read_only_fields = ['deposit']
+
+class SavingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Saving
+        fields = '__all__'
+
+class SavingOptionsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = SavingOptions
+        fields = '__all__'
+        read_only_fields = ['saving']
--- final_pjt_back/final_pjt_back/urls.py
+++ final_pjt_back/final_pjt_back/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('banks.urls')),
+    path('api/bank/', include('banks.urls')),
+    path('api/article/', include('articles.urls')),
     path('accounts', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
