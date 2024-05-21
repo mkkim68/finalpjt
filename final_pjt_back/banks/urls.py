49c42ec7c2cd82c1e3df9210b03a948b5ff6b40e
뭉쿠키 <jamie9@naver.com>
2024-05-21 17:37:50 +0900

    alal

--- final_pjt_back/banks/urls.py
+++ final_pjt_back/banks/urls.py
@@ -2,13 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #예금
     path('deposits/', views.deposit_list),
     path('deposit-options/', views.deposit_option_list),
     path('deposits/<str:fin_prdt_cd>/', views.deposit_detail),
+    path('deposits/<str:fin_prdt_cd>/', views.deposit_detail),
+    path('deposits/<str:fin_prdt_cd>/option/', views.deposit_detail_option),
+    path('deposits/<str:fin_prdt_cd>/join/', views.deposit_detail_join),
+    #적금
     path('savings/', views.saving_list),
     path('saving-options/', views.saving_option_list),
     path('savings/<str:fin_prdt_cd>/', views.saving_detail),
+    path('savings/<str:fin_prdt_cd>/option/', views.saving_detail_option),
+    #환율
     path('exchange/', views.exchange),
     path('local/exchange/', views.get_local_exchange, name='get_local_exchange'),
+    #지도
     path('banks/', views.bank_list),
 ]
